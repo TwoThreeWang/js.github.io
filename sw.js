61b6e1ec6711dea36143667b2c15ce619f31a51e
王图思睿 <hi_wangcheng@163.com>
2019-07-06 09:17:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/bundle.fc09445b.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.fc09445b.js')};importScripts('conf.js')
+
+//importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/bundle.fc09445b.js')
